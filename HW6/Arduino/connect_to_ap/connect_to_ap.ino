aa4ee4cb1c315b8907fce1afcb25b019cc9b6fd4
agzhang49 <91279867+agzhang49@users.noreply.github.com>
2023-06-03 17:51:43 -0700

    arduino files for hw 6

---
+++ HW6/Arduino/connect_to_ap/connect_to_ap.ino
@@ -0,0 +1,30 @@
+#include <WiFi.h>
+const char* ssid = "Andy";   /*Replace SSID of your network*/
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);  /*Initialize ESP32 WiFi in station mode*/
+  WiFi.begin(ssid); /*Begin WiFi connection*/
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());  /*Print local IP address of ESP32*/
+}
+void setup() {
+  Serial.begin(115200); /*Baud Rate for serial communication*/
+  initWiFi();  /*Initialize WiFi*/
+  
+}
+void loop() {
+  Serial.print("Connected Network Signal Strength (RSSI): ");
+  Serial.println(WiFi.RSSI());  /*Print WiFi signal strength*/
+  delay(200);
+  if(int(WiFi.RSSI())>-70){
+    Serial.println("Cat is near the door");
+
+  }
+  else{
+    Serial.println("Cat is far");
+  }
+}
---
+++ HW6/Arduino/find_address/find_address.ino
@@ -0,0 +1,48 @@
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+ 
+bool initBluetooth()
+{
+  if (!btStart()) {
+    Serial.println("Failed to initialize controller");
+    return false;
+  }
+ 
+  if (esp_bluedroid_init() != ESP_OK) {
+    Serial.println("Failed to initialize bluedroid");
+    return false;
+  }
+ 
+  if (esp_bluedroid_enable() != ESP_OK) {
+    Serial.println("Failed to enable bluedroid");
+    return false;
+  }
+ 
+}
+ 
+void printDeviceAddress() {
+ 
+  const uint8_t* point = esp_bt_dev_get_address();
+ 
+  for (int i = 0; i < 6; i++) {
+ 
+    char str[3];
+ 
+    sprintf(str, "%02X", (int)point[i]);
+    Serial.print(str);
+ 
+    if (i < 5){
+      Serial.print(":");
+    }
+ 
+  }
+}
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  initBluetooth();
+  printDeviceAddress();
+}
+ 
+void loop() {}
