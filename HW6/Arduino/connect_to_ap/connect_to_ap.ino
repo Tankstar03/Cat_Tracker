7514fda515437993cfaab3fe8dee0c434fcc7557
agzhang49 <91279867+agzhang49@users.noreply.github.com>
2023-06-09 15:26:02 -0700

    anyd

--- HW6/Arduino/connect_to_ap/connect_to_ap.ino
+++ HW6/Arduino/connect_to_ap/connect_to_ap.ino
@@ -1,9 +1,12 @@
 #include <WiFi.h>
 const char* ssid = "Andy";   /*Replace SSID of your network*/
+// const char* ssid2 = "Jason";   /*Replace SSID of your network*/
+
 
 void initWiFi() {
   WiFi.mode(WIFI_STA);  /*Initialize ESP32 WiFi in station mode*/
   WiFi.begin(ssid); /*Begin WiFi connection*/
+  // WiFi.begin(ssid2); /*Begin WiFi connection*/
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
@@ -17,14 +20,17 @@ void setup() {
   
 }
 void loop() {
-  Serial.print("Connected Network Signal Strength (RSSI): ");
+  Serial.print("Connected Network Signal Strength (Jason)(RSSI): ");
   Serial.println(WiFi.RSSI());  /*Print WiFi signal strength*/
+  // delay(50);
+  // Serial.print("Connected Network Signal Strength (Andy)(RSSI): ");
+  // Serial.println(WiFi.RSSI(*ssid2));  /*Print WiFi signal strength*/
   delay(200);
-  if(int(WiFi.RSSI())>-70){
-    Serial.println("Cat is near the door");
+  // if(int(WiFi.RSSI())>-70){
+  //   Serial.println("Cat is near the door");
 
-  }
-  else{
-    Serial.println("Cat is far");
-  }
+  // }
+  // else{
+  //   Serial.println("Cat is far");
+  // }
 }
